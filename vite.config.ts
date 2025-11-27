e3b7ccea72a11ca345d3abca313dc1291128748f
unknown <harshitasrikakolapu@gmail.com>
2025-11-27 21:59:55 +0530

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -6,27 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Bible Verses for Every Mood',
-        short_name: 'Bible Moods',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
+      registerType: 'autoUpdate',   // this is enough
     })
   ]
 })
